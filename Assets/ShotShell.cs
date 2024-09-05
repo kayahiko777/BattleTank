7c54d1741c2caed4141ea055cbc412efeae131f5
kayahiko777 <kayahiko777@gmail.com>
2024-09-05 23:23:49 +0900

    Game Weapon Icon

--- Assets/ShotShell.cs
+++ Assets/ShotShell.cs
@@ -1,25 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ShotShell : MonoBehaviour
 {
     public float shotSpeed;
     public GameObject ShellPrefab;
     public AudioClip shotSound;
+    private float interval = 0.75f;
+    private float timer = 0;
+    public int shotCount;
+    public TextMeshProUGUI shellLabel;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        shellLabel.text = "" + shotCount;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // タイマーの時間を動かす
+        timer += Time.deltaTime;
         // もしもSpaceキーを押したならば（条件）
         // 「Space」の部分を変更することで他のキーにすることができる（ポイント）
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&& timer > interval&& shotCount > 0)
         {
+            shotCount -= 1;
+            shellLabel.text ="" + shotCount;
+            // タイマーの時間を０に戻す
+            timer = 0.0f;
             // 砲弾のプレハブを実体化（インスタンス化）する
             GameObject shell = Instantiate(ShellPrefab,transform.position,Quaternion.identity);
             // 砲弾に付いているRigidbodyコンポーネントにアクセスする。
